9bc75ff504f820a508577d7f2b315312ecbafab7
revuwem <revuwem@gmail.com>
2020-10-06 16:12:45 +0300

    lists module created

---
+++ src/components/list/index.js
@@ -0,0 +1,9 @@
+import InfoList from './info-list';
+import RulesList from './rules-list';
+import ResponsesList from './responses-list';
+
+export {
+    InfoList,
+    RulesList,
+    ResponsesList
+}
