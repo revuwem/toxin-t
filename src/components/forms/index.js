60cc7b3205d10aabc578f124233651d9d829ccb3
revuwem <revuwem@gmail.com>
2020-10-06 17:29:21 +0300

    forms module created

---
+++ src/components/forms/index.js
@@ -0,0 +1,11 @@
+import BookingForm from './booking-form';
+import FilterForm from './filter-form';
+import SearchRoomForm from './search-room-form';
+import UserRegForm from './user-reg-form';
+
+export {
+    BookingForm, 
+    FilterForm,
+    SearchRoomForm,
+    UserRegForm
+}
