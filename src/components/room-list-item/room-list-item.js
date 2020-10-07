610d8b2c52f1cfd82635db759025e492a03a50a0
revuwem <revuwem@gmail.com>
2020-10-07 11:17:22 +0300

    added room data rendering

--- src/components/room-list-item/room-list-item.js
+++ src/components/room-list-item/room-list-item.js
@@ -2,24 +2,28 @@ import React from 'react';
 
 import './room-list-item.scss';
 
-const RoomListItem = () => {
+const RoomListItem = ({room}) => {
+
+    const {id, prestige, price, rating, tileImage, responses } = room;
+    const responsesCount = responses.reduce((acc, idx) => acc+1, 0);
+
     return (
         <li className="room-list-item">
             <div className="room-list-item__gallery">
-                <img src="/images/rooms/image.png" alt="Двухспальный номер с кондиционером" />
+                <img src={tileImage} alt="" />
             </div>
             <div className="room-list-item__wrapper">
                 <div className="room-list-item__inner">
-                    <h3>№ 840</h3>
-                    <p><span>9 900 Р</span> в сутки</p>
+                    <h3>№ {id} <span>{prestige}</span></h3>
+                    <p><span>{price} Р</span> в сутки</p>
                 </div>
                 <hr />
                 <div className="room-list-item__inner">
                     <a href="" className="room-list-item__link">
-                        <img src="/images/rating/4.png" alt="4 звезды" />
+                        <img src={`/images/rating/${rating}.png`} alt={`${rating} звезды`} />
                     </a>
                     <a href="#" className="room-list-item__link">
-                        <p><span>65</span> отзывов</p>
+                        <p><span>{responsesCount}</span> отзывов</p>
                     </a>
                 </div>
             </div>
