321a9802d3058f1b25d4d4e7c38f478f112682be
revuwem <revuwem@gmail.com>
2020-10-07 17:26:02 +0300

    components assembled, page styled

--- src/components/pages/rooms-page/rooms-page.js
+++ src/components/pages/rooms-page/rooms-page.js
@@ -1,12 +1,27 @@
 import React from 'react';
 
+import { FilterForm } from '../../forms';
+import RoomList from '../../room-list';
+
 import './rooms-page.scss';
+import RoomsService from '../../../services/rooms-service';
+
 
 const RoomsPage = () => {
+
+    const roomsService = new RoomsService();
+    const rooms = roomsService.getData();
+
     return (
         <main className="page" id="rooms-page">
             <div className="page__wrapper">
-                <p>RoomsPage</p>
+                <aside>
+                    <FilterForm />
+                </aside>
+                <section>
+                    <h2>Номера, которые мы для вас подобрали</h2>
+                    <RoomList rooms={rooms} />
+                </section>
             </div>
         </main>
 
