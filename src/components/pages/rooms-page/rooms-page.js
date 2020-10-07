15aa4e33da0bafe9c735e9bd0da22f50cef8b797
revuwem <revuwem@gmail.com>
2020-10-07 10:40:46 +0300

    RoomsPage component blank created

---
+++ src/components/pages/rooms-page/rooms-page.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './rooms-page.scss';
+
+const RoomsPage = () => {
+    return (
+        <main className="page" id="rooms-page">
+            <div className="page__wrapper">
+                <p>RoomsPage</p>
+            </div>
+        </main>
+
+    );
+}
+
+export default RoomsPage;
